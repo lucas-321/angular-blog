61efeb6914deb95b30cdc35a954b91d8ced7162e
Lucas Reis <lucas_klsr@hotmail.com>
2023-05-23 19:32:34 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"Saiu novo Filme",
+    "description":"Olá, Mundão!",
+    "photoCover": "https://d2d7ho1ae66ldi.cloudfront.net/ArquivoNoticias/fccc6398-0f8f-11ed-aa6e-9587410378a2/tony-stark-snap.jpg"
+  },
+  {
+    "id":"2",
+    "title":"Nova série anunciada no Disney+",
+    "description":"Olá, Terra!",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  }
+]
